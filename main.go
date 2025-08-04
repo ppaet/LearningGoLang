0b319a2789c94fb97ff37875a8ccb7267d2245ec
ZekeRuan <rzy1147994141@gmail.com>
2025-08-04 11:37:04 +0800

    .

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	fruits := []int{1, 0, 1, 4, 1, 4, 1, 2, 3}
+	fruits := []int{0, 0, 1, 1}
 	totalFruit(fruits)
 	os.Exit(0)
 }
@@ -41,38 +41,46 @@ func generate(numRows int) [][]int {
 	return ret
 }
 
-// [1,0,1,4,1,4,1,2,3]
 func totalFruit(fruits []int) int {
 	t1, t2, t3 := -1, -1, -1
 	now, n2, n3 := 0, 0, 0
-	max := 0
-
+	maxN := 0
 	for i := 0; i < len(fruits); i++ {
 		if t3 == fruits[i] {
 			n3++
-			now++
+			if t3 == t1 {
+				now++
+			} else {
+				now = 1
+			}
+
 		} else if t2 == fruits[i] {
 			n2++
-			now = 0
+			if t2 == t1 {
+				now++
+			} else {
+				now = 1
+			}
 		} else {
-			now = 0
-			if n2+n3 > max {
-				max = n2 + n3
+			if n2+n3 > maxN {
+				maxN = n2 + n3
 			}
 			if t1 == t3 && t1 != -1 {
 				t2 = fruits[i]
 				n2 = 1
+				n3 = now
 			} else {
 				t3 = fruits[i]
 				n3 = 1
+				n2 = now
 			}
+			now = 1
 		}
-
 		t1 = fruits[i]
 	}
-	if n2+n3 > max {
-		max = n2 + n3
+	if n2+n3 > maxN {
+		maxN = n2 + n3
 	}
-	fmt.Println("Max fruits:", max)
-	return max
+	fmt.Println("Max fruits:", maxN)
+	return maxN
 }
